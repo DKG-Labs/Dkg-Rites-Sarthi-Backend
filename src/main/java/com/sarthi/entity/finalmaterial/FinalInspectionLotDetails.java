6080ad992ce64a1ef519e4e6c4e25fb7434a6ae8
kudaykiran-99 <uday@dkgrouplabs.com>
2026-02-26 18:23:23 +0530

    bug fixed

--- src/main/java/com/sarthi/entity/finalmaterial/FinalInspectionLotDetails.java
+++ src/main/java/com/sarthi/entity/finalmaterial/FinalInspectionLotDetails.java
@@ -45,6 +45,7 @@ public class FinalInspectionLotDetails {
     // ---- ADDITIONAL TRACKING ----
     private Long processIcId;
     private String processIcNumber;
+    private Integer noOfBags;
 
     // ---- TIMESTAMPS ----
     private LocalDateTime createdAt;
