a3255fb85487e78adf0d36752004f1bcf9547e6c
kudaykiran-99 <uday@dkgrouplabs.com>
2026-03-19 12:45:47 +0530

    changes api

--- src/main/java/com/sarthi/Sleeper/controller/LongLineController.java
+++ src/main/java/com/sarthi/Sleeper/controller/LongLineController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/longLine-bench")
+@RequestMapping("/api/longLine-bench")
 public class LongLineController {
 
     @Autowired
--- src/main/java/com/sarthi/Sleeper/controller/StressBenchMasterController.java
+++ src/main/java/com/sarthi/Sleeper/controller/StressBenchMasterController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/stress-bench")
+@RequestMapping("/api/stress-bench")
 public class StressBenchMasterController {
 
 
