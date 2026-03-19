aa42aa22df10027703d2f260f3a50a62eec6408d
kudaykiran-99 <uday@dkgrouplabs.com>
2026-03-19 13:52:36 +0530

    change

--- src/main/java/com/sarthi/repository/RoleMasterRepository.java
+++ src/main/java/com/sarthi/repository/RoleMasterRepository.java
@@ -1,5 +1,6 @@
 package com.sarthi.repository;
 
+import aj.org.objectweb.asm.commons.Remapper;
 import com.sarthi.entity.RoleMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,6 @@ public interface RoleMasterRepository extends JpaRepository<RoleMaster, Integer>
 
 
      Optional<RoleMaster> findByRoleName(String roleName);
+
+    Optional<RoleMaster> findByRoleId(Integer roleId);
 }
