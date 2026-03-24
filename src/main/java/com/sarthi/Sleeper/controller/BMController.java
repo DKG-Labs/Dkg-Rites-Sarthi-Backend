51162c6eb12dd85a29f4f7165e44d4cf6c11ff0d
kudaykiran-99 <uday@dkgrouplabs.com>
2026-03-24 14:32:06 +0530

    workflow integration

--- src/main/java/com/sarthi/Sleeper/controller/BMController.java
+++ src/main/java/com/sarthi/Sleeper/controller/BMController.java
@@ -3,6 +3,7 @@ package com.sarthi.Sleeper.controller;
 import com.sarthi.Sleeper.dto.BenchMouldLongStrssDtos.BMRequestDTO;
 import com.sarthi.Sleeper.dto.BenchMouldLongStrssDtos.BMResponseDTO;
 import com.sarthi.Sleeper.service.BMService;
+import com.sarthi.Sleeper.service.SleeperWorkflowService;
 import com.sarthi.util.ResponseBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,8 @@ public class BMController {
 
     @Autowired
     private BMService bmService;
+    @Autowired
+    private SleeperWorkflowService sleeperWorkflowService;
 
 
     @PostMapping("/create")
@@ -24,6 +27,10 @@ public class BMController {
             @RequestBody BMRequestDTO dto) {
 
         BMResponseDTO result = bmService.create(dto);
+        String requestId = String.valueOf(result.getId());
+        Long md = 2L;
+        Long wid = 1L;
+        sleeperWorkflowService.initiateWorkflow(requestId,md, wid, Long.valueOf(result.getCreatedBy()));
 
         return new ResponseEntity<>(
                 ResponseBuilder.getSuccessResponse(result),
