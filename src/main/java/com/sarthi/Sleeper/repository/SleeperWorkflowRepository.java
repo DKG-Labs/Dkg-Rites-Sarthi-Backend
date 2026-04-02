1e2cf5b84918ee24a68c42d432ac8e655e6d13ca
kudaykiran-99 <uday@dkgrouplabs.com>
2026-04-02 14:14:41 +0530

    workflow change

--- src/main/java/com/sarthi/Sleeper/repository/SleeperWorkflowRepository.java
+++ src/main/java/com/sarthi/Sleeper/repository/SleeperWorkflowRepository.java
@@ -18,17 +18,30 @@ public interface SleeperWorkflowRepository
 
     List<SleeperWorkflowTransaction> findByRequestIdOrderByCreatedDateAsc(String requestId);
 
-    @Query("""
-                SELECT t FROM SleeperWorkflowTransaction t
-                WHERE t.workflowTransitionId = (
-                    SELECT MAX(t2.workflowTransitionId)
-                    FROM SleeperWorkflowTransaction t2
-                    WHERE t2.requestId = t.requestId
-                )
-                AND t.status IN ('CREATED','PENDING')
-                AND t.nextRole = :roleName
-            """)
-    List<SleeperWorkflowTransaction> findLastPendingRequestsByRole(String roleName);
+   /* @Query("""
+    SELECT t FROM SleeperWorkflowTransaction t
+    WHERE t.workflowTransitionId = (
+        SELECT MAX(t2.workflowTransitionId)
+        FROM SleeperWorkflowTransaction t2
+        WHERE t2.requestId = t.requestId
+        AND t2.status IN ('Created','PENDING')
+    )
+    AND t.nextRole = :roleName
+""")
+    List<SleeperWorkflowTransaction> findLastPendingRequestsByRole(String roleName); */
+   @Query("""
+    SELECT t FROM SleeperWorkflowTransaction t
+    WHERE t.workflowTransitionId = (
+        SELECT MAX(t2.workflowTransitionId)
+        FROM SleeperWorkflowTransaction t2
+        WHERE t2.requestId = t.requestId
+        AND t2.moduleId = t.moduleId  
+        AND t2.status IN ('Created','PENDING')
+    )
+    AND t.status IN ('Created','PENDING')
+    AND t.nextRole = :roleName
+""")
+   List<SleeperWorkflowTransaction> findLastPendingRequestsByRole(String roleName);
 
     @Query("""
                 SELECT t FROM SleeperWorkflowTransaction t
