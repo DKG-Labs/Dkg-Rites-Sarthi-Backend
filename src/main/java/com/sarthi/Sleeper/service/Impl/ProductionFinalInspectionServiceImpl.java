af8f360cafb0f3e12225196ee77bc5b73ea43c4d
kudaykiran-99 <uday@dkgrouplabs.com>
2026-04-06 12:04:57 +0530

    changes

--- src/main/java/com/sarthi/Sleeper/service/Impl/ProductionFinalInspectionServiceImpl.java
+++ src/main/java/com/sarthi/Sleeper/service/Impl/ProductionFinalInspectionServiceImpl.java
@@ -123,6 +123,7 @@ public class ProductionFinalInspectionServiceImpl implements ProductionFinalInsp
       header.setTestDate(LocalDate.now());
       header.setCreatedDate(LocalDateTime.now());
 
+      header.setSleeperType(dto.getSleeperType());
       headerRepository.save(header);
 
       Map<Long, InspectionParameter> parameterMap =
@@ -775,13 +776,14 @@ public List<BatchTestingListResponseDto> getAllBatchTesting(Long moduleId) {
     public List<BatchInspectionResponseDto> getCompletedBatches(String sleeperType, String userId) {
 
         String parsedUserId = userId.replace(":", "");
-        Long vendorId = Long.parseLong(parsedUserId);
+      //  Long vendorId = Long.parseLong(parsedUserId);
 
         Optional<UserMaster> userOpt = userMasterRepository.findByUserName(userId);
         if (userOpt.isEmpty()) {
             userOpt = userMasterRepository.findByUserName(parsedUserId);
         }
 
+        Long vendorId =0L;
         if (userOpt.isPresent()) {
             vendorId = userOpt.get().getUserId().longValue();
         }
