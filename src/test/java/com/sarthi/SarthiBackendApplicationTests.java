0caf9fc141ff75e41870f9461eeb9d118bf49ad3
Kishore <kishore@dkgrouplabs.com>
2026-08-19 16:43:16 +0530

    deployment bug

--- src/test/java/com/sarthi/SarthiBackendApplicationTests.java
+++ src/test/java/com/sarthi/SarthiBackendApplicationTests.java
@@ -1,10 +1,8 @@
 package com.sarthi;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled("Context load test disabled during CI/CD builds without live test database")
 @SpringBootTest
 class SarthiBackendApplicationTests {
 
